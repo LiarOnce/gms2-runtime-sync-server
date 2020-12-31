c55caff67526f78345fab66882a113907df43c98
LiarOnce <qq1062787843@hotmail.com>
2020-12-31 21:52:18 +0800

    Modify the method of getting modules' url.

--- cleanfile.js
+++ cleanfile.js
@@ -4,5 +4,6 @@ const shell = require("shelljs");
 
 if (fs.access("./result/",fs.constants.F_OK, function(err) {
     shell.exec("rm -rf ./results/*");
-    shell.exec("rm -rf ./rss/*")
+    shell.exec("rm -rf ./rss/*");
+    shell.exec("rm -rf ./mirror/*");
 }));
--- generateJSON.js
+++ generateJSON.js
@@ -27,6 +27,7 @@ function parseRSS() {
         let jsonresult = JSON.stringify(result, null, "\t");
         fs.writeFile("./results/result.json", jsonresult, (err) => {
             console.log("Convert XML to JSON successfully.")
+            minimalRSS();
         });
     });
 }
@@ -39,47 +40,88 @@ function cutTheNewestVersion() {
     shell.cd("..");
 }
 
-function cutModuleFromJSON() {
-    shell.cd("results/runtime");
-    shell.exec("sed -i '1d' modules.json");
-    shell.exec("sed -i '$d' modules.json");
-    shell.exec("sed -n '1, 9p' modules.json> modules/android.json");
-    shell.exec("sed -n '10, 18p' modules.json> modules/html5.json");
-    shell.exec("sed -n '19, 27p' modules.json> modules/ios.json");
-    shell.exec("sed -n '28, 36p' modules.json> modules/linux.json");
-    shell.exec("sed -n '37, 45p' modules.json> modules/linuxYYC.json");
-    shell.exec("sed -n '46, 54p' modules.json> modules/mac.json");
-    shell.exec("sed -n '55, 63p' modules.json> modules/macYYC.json");
-    shell.exec("sed -n '64, 72p' modules.json> modules/ps4.json");
-    shell.exec("sed -n '73, 81p' modules.json> modules/switch.json");
-    shell.exec("sed -n '82, 90p' modules.json> modules/tvos.json");
-    shell.exec("sed -n '91, 99p' modules.json> modules/windows.json");
-    shell.exec("sed -n '100, 108p' modules.json> modules/windowsuap.json");
-    shell.exec("sed -n '109, 117p' modules.json> modules/windowsYYC.json");
-    shell.exec("sed -n '118, 126p' modules.json> modules/xboxone.json");
-    shell.cd("modules");
-    shell.ls('*.json').forEach(function (file) {
-        shell.sed('-i', '},', '}', file);
-    });
-}
-
 function cutModule() {
     fs.access("./results/result3.json", fs.constants.F_OK, function (err) {
         if (!err) {
             let result3JSON = fs.readFileSync("./results/result3.json");
+            fs.mkdirSync("./results/runtime");
+            //Get comment and enclosure
             let modulesJSON = JSON.parse(result3JSON);
             let comment = modulesJSON.comments;
-            fs.writeFile("./results/runtime/modules/edition/comment.txt", comment, (err)=>{
+            fs.writeFile("./results/runtime/comment.txt", comment, (err)=>{
                 console.log("Get comment successfully.");
             });
             let enclosure = modulesJSON.enclosure.$.url;
-            fs.writeFile("./results/runtime/modules/enclosure.txt", enclosure, (err)=>{
+            fs.writeFile("./results/runtime/enclosure.txt", enclosure, (err)=>{
                 console.log("Get enclosure successfully.");
             });
-            let cuttedModule = JSON.stringify(modulesJSON.enclosure.module, null, "\t");
-            fs.writeFile("./results/runtime/modules.json", cuttedModule, (err) => {
-                cutModuleFromJSON();
-                console.log("Cut the module from JSON successfully.");
+            //Get modules
+            fs.mkdirSync("./results/runtime/modules");
+            
+            //For Desktop
+            let linux = modulesJSON.enclosure.module[3].$.url;
+            fs.writeFile("./results/runtime/modules/linux.txt", linux, (err)=>{
+                console.log("Get (Desktop) Linux modules successfully.");
+            });
+            let linuxYYC = modulesJSON.enclosure.module[4].$.url;
+            fs.writeFile("./results/runtime/modules/linuxYYC.txt", linuxYYC, (err)=>{
+                console.log("Get (Desktop) Linux (YoYo Compiler) modules successfully.");
+            });
+            let mac = modulesJSON.enclosure.module[5].$.url;
+            fs.writeFile("./results/runtime/modules/mac.txt", mac, (err)=>{
+                console.log("Get (Desktop) macOS modules successfully.");
+            });
+            let macYYC = modulesJSON.enclosure.module[6].$.url;
+            fs.writeFile("./results/runtime/modules/macYYC.txt", macYYC, (err)=>{
+                console.log("Get (Desktop) macOS (YoYo Compiler) modules successfully.");
+            });
+            let windows = modulesJSON.enclosure.module[10].$.url;
+            fs.writeFile("./results/runtime/modules/windows.txt", windows, (err)=>{
+                console.log("Get (Desktop) Windows modules successfully.");
+            });
+            let windowsYYC = modulesJSON.enclosure.module[12].$.url;
+            fs.writeFile("./results/runtime/modules/windowsYYC.txt", windowsYYC, (err)=>{
+                console.log("Get (Desktop) Windows (YoYo Compiler) modules successfully.");
+            });
+
+            //For Mobile
+            let android = modulesJSON.enclosure.module[0].$.url;
+            fs.writeFile("./results/runtime/modules/android.txt", android, (err)=>{
+                console.log("Get (Mobile) Android modules successfully.");
+            });
+            let ios = modulesJSON.enclosure.module[2].$.url;
+            fs.writeFile("./results/runtime/modules/ios.txt", ios, (err)=>{
+                console.log("Get (Mobile) iOS modules successfully.");
+            });
+            let tvos = modulesJSON.enclosure.module[9].$.url;
+            fs.writeFile("./results/runtime/modules/tvos.txt", tvos, (err)=>{
+                console.log("Get (Mobile) tvOS module successfully.")
+            });
+
+            //For Web
+            let html5 = modulesJSON.enclosure.module[1].$.url;
+            fs.writeFile("./results/runtime/modules/html5.txt", html5, (err)=>{
+                console.log("Get (Web) HTML5 modules successfully.");
+            });
+            
+            //For UWP
+            let windowsuap = modulesJSON.enclosure.module[11].$.url;
+            fs.writeFile("./results/runtime/modules/windowsuap.txt", windowsuap, (err)=>{
+                console.log("Get (UWP) Windows 10 UWP modules successfully.");
+            });
+
+            //For console
+            let ps4 = modulesJSON.enclosure.module[7].$.url;
+            fs.writeFile("./results/runtime/modules/ps4.txt", ps4, (err)=>{
+                console.log("Get (Console) PlayStation 4 modules successfully.");
+            });
+            let nswitch = modulesJSON.enclosure.module[8].$.url;
+            fs.writeFile("./results/runtime/modules/nswitch.txt", nswitch, (err)=>{
+                console.log("Get (Console) Nintendo Switch modules successfully.");
+            });
+            let xboxone = modulesJSON.enclosure.module[13].$.url;
+            fs.writeFile("./results/runtime/modules/xboxone.txt", xboxone, (err)=>{
+                console.log("Get (Console) Xbox One modules successfully.");
             });
         } else {
             console.log(err);
@@ -126,7 +168,6 @@ fs.access("./rss/" + RSS.RSSFile, fs.constants.F_OK, function (err) {
                 console.log("Download successfully. \nIf you want to change channel, you need run 'npm run clean' to delete generated files.")
                 console.log("And change 'channel' in 'configs/config.json', you also need to run 'npm run config' to take effect.\n")
                 parseRSS();
-                minimalRSS();
             });
         }
     } else {
