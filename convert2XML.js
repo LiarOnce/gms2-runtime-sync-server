58db5a2602cd2cd2e3e716453815add547b8989e
LiarOnce <qq1062787843@hotmail.com>
2021-01-02 19:29:05 +0800

    Clean obsolete code.

--- convert2XML.js
+++ convert2XML.js
@@ -80,19 +80,4 @@ let final = result3String.split(runtimeOriginalURL).join(runtimeMirrorURL);
 fs.writeFile("./mirror/latest.json", final, (err) => {
     console.log("Change Mirror successfully.");
     download();
-    //generateXML();
 });
-
-// fs.access("./mirror/latest.json", fs.constants.F_OK, (err) => {
-//     let result = require("./results/result.json");
-//     let latest = "[" + JSON.parse(fs.readFileSync("./mirror/latest.json")) + "]";
-//     let resultAfter = result.rss.channel.item[result.rss.channel.item.length - 1];
-//     resultAfter.push(latest);
-//     fs.writeFile("./mirror/runtime.json", resultAfter, (err) => {
-//         console.log("Write JSON successfully.")
-//     });
-// });
-//shell.cd("mirror");
-//shell.exec("sed -i '1d' latest.xml");
-
-//let latest = fs.readFileSync("latest.xml");
