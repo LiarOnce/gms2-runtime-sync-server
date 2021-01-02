a457ddc978d21adf1dbaf9c68f8c086d84498172
LiarOnce <qq1062787843@hotmail.com>
2021-01-02 19:29:05 +0800

    Clean obsolete code.

--- readcomment.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-let result3JSON = fs.readFileSync("./results/result3.json");
-let modulesJSON = JSON.parse(result3JSON);
-let comment = modulesJSON.comments;
-
-fs.writeFile("./results/runtime/modules/edition/comment.txt", comment, (err)=>{
-    console.log("Get comment successfully.");
-});
