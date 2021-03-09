6916c1c3dfc6225c5cf6554b8158a243b3fa53b2
LiarOnce <qq1062787843@hotmail.com>
2021-03-09 10:47:16 +0800

    cleanfile.

--- cleanfile.js
+++ cleanfile.js
@@ -1,9 +1,15 @@
 "use strict";
+const { constants } = require("buffer");
 const fs = require("fs");
 const shell = require("shelljs");
 
-if (fs.access("./result/",fs.constants.F_OK, function(err) {
+fs.access("./results/", fs.constants.F_OK, (err)=>{
     shell.exec("rm -rf ./results/*");
     shell.exec("rm -rf ./rss/*");
+    console.log("Clean RSS cache successfully.");
+});
+
+fs.access("./mirror/", fs.constants.F_OK, (err)=>{
     shell.exec("rm -rf ./mirror/*");
-}));
+    console.log("Clean RSS Mirror cache successfully.");
+});
